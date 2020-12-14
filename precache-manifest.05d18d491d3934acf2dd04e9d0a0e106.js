4508d33cbef26ed15cef06a55e86b06f12badc37
Kapoor <akshkapoor@deloitte.com>
2020-12-14 18:51:56 +0530

    Updates

---
+++ precache-manifest.05d18d491d3934acf2dd04e9d0a0e106.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a2e6d3e18d3af13d40aeb83e889d0b",
+    "url": "/FaceRecognition-pages/index.html"
+  },
+  {
+    "revision": "34c7967a367e4e6749fe",
+    "url": "/FaceRecognition-pages/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f251fa3706f844baf374",
+    "url": "/FaceRecognition-pages/static/css/main.c9621e98.chunk.css"
+  },
+  {
+    "revision": "34c7967a367e4e6749fe",
+    "url": "/FaceRecognition-pages/static/js/2.717b2039.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition-pages/static/js/2.717b2039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f251fa3706f844baf374",
+    "url": "/FaceRecognition-pages/static/js/main.e484cd46.chunk.js"
+  },
+  {
+    "revision": "f30ec2cd4dcb91918f93",
+    "url": "/FaceRecognition-pages/static/js/runtime-main.11b40c68.js"
+  },
+  {
+    "revision": "eff55aa9ff940cdf7d01eb505d81c632",
+    "url": "/FaceRecognition-pages/static/media/logo.eff55aa9.png"
+  }
+]);
